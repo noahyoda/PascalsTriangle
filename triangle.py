92ef3ff988fcd753096d6cbd45386bc84f09f98f
noahyoda <noahyoda1@gmail.com>
2021-09-19 21:43:00 -0700

    added basic functions for printing and updating logic

--- triangle.py
+++ triangle.py
@@ -5,9 +5,15 @@ line = [0] * size
 line[len(line) - 1] = 1
 
 while i < size:
-	for x in line:
-		if x > 0:
-			print(x)
+	printarr(line)
 
-def
+def printarr(arr):
+	index = 0
+	while index < len(arr):
+		if arr[index] > 0:
+			print(arr[index])
 
+def updatearr(arr):
+	index = 0
+	while index < len(arr) - 1:
+		arr[index] = arr[index] + arr[index+ 1]
