11df7aa994ee7c48aa614fcbd23c57bc179676c8
noahyoda <noahyoda1@gmail.com>
2021-09-19 21:56:18 -0700

    logic complete, program works as desired!

--- triangle.py
+++ triangle.py
@@ -4,16 +4,22 @@ i = 0
 line = [0] * size
 line[len(line) - 1] = 1
 
-while i < size:
-	printarr(line)
-
 def printarr(arr):
 	index = 0
 	while index < len(arr):
 		if arr[index] > 0:
-			print(arr[index])
+			print(arr[index]),
+		index += 1
 
 def updatearr(arr):
 	index = 0
 	while index < len(arr) - 1:
 		arr[index] = arr[index] + arr[index+ 1]
+		index += 1
+
+while i < size:
+	printarr(line)
+	print
+	updatearr(line)
+	i += 1
+
